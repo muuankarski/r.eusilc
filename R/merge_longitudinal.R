d73ef320281b0821fa3dc66c2d31243829633700
Markus Kainu <markuskainu@gmail.com>
2014-02-17 11:30:29 +0200

    longitudinal merging works. using read.csv instead fread. Favouring simplicity & reliability over speed this time.

--- R/merge_longitudinal.R
+++ R/merge_longitudinal.R
@@ -10,10 +10,10 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-#' Merge eu-silc cross-sectional and longitudinal raw .csv datasets
+#' Merge eu-silc personal level longitudinal raw .csv datasets, personal_register and personal_data
 #'
 #' @param origin.path A string. Specify the path where to load the original csv files.
-#' @param destination.path A string. Specify the path where to save the merged file.
+#' @param destination.path A string. Specify the path where to save the merged file. value=/code{"not_save"} does not save the merged file
 #'
 #' @return data.frame
 #'
@@ -21,38 +21,173 @@
 #' @examples # merge_longitudinal(origin.path = "~/eusilc_raw", destination.path="~/eusilc_merged")
 #' @author Markus Kainu <markuskainu(at)gmail.com> 
 
-merge_longitudinal <- function(origin.path,destination.path) {
 
-# household register 
-hh_reg <- read.csv("origin.path/d_file.csv", header=TRUE)
-hh_reg$HH_ID_Y <- factor(paste(hh_reg$DB010,hh_reg$DB020,hh_reg$DB030, sep="_"))
-hh_reg$HH_ID <- factor(paste(hh_reg$DB020,hh_reg$DB030, sep="_"))
-# household data
-hh_data <- read.csv("origin.path/h_file.csv", header=TRUE)
-hh_data$HH_ID_Y <- factor(paste(hh_data$HB010,hh_data$HB020,hh_data$HB030, sep="_"))
-hh_data$HH_ID <- factor(paste(hh_data$HB020,hh_data$HB030, sep="_"))
+merge_longi_personal <- function(origin.path,destination.path,format) {
+  
+  if(!exists("origin.path")) stop("origin.path not defined")
+  if(!exists("destination.path")) stop("destination.path not defined")
+  if(!exists("format")) stop("format not defined")
+  if(!(format %in% c("csv","RData","spss","sas","stata"))) stop("Wrong format. Use csv,RData,spss,sas,stata")
+  
+  # suppressPackageStartupMessages(library(data.table))
+  # suppressPackageStartupMessages(library(bit64))
+  
+  
 # personal register
-per_reg <- read.csv("origin.path/r_file.csv", header=TRUE)
+path_personal_register <- paste(origin.path,"/r_file.csv",sep="")
+# per_reg <- fread(path_personal_register, header = T, sep = ',')
+per_reg <- read.csv(path_personal_register, header = T, sep = ',')
 per_reg$PER_ID_Y <- factor(paste(per_reg$RB010,per_reg$RB020,per_reg$RB030, sep="_"))
 per_reg$PER_ID <- factor(paste(per_reg$RB020,per_reg$RB030, sep="_"))
 # personal data
-per_data <- read.csv("origin.path/p_file.csv", header=TRUE)
+path_personal_data <- paste(origin.path,"/p_file.csv",sep="")
+# per_data <- fread(path_personal_data, header = T, sep = ',')
+per_data <- read.csv(path_personal_data, header = T, sep = ',')
 per_data$PER_ID_Y <- factor(paste(per_data$PB010,per_data$PB020,per_data$PB030, sep="_"))
 per_data$PER_ID <- factor(paste(per_data$PB020,per_data$PB030, sep="_"))
+
 #--------------------------------------------------------------------#
 # merge data
-# merge household datas
-library(plyr)
-hh_merge <- join(hh_reg,hh_data,by="HH_ID_Y", type="full")
-# merge personal datas
-per_merge <- join(per_reg,per_data,by="PER_ID_Y", type="full")
-#per_merge$HH_ID_Y <- factor(paste(per_merge$RB010,per_merge$RB020,per_merge$PX030, sep="_"))
-# merge household and personal into single data
-#merge <- join(hh_merge,per_merge,by="HH_ID_Y", type="full")
+per_merge <- merge(per_reg,per_data,by="PER_ID_Y", all=TRUE)
 #--------------------------------------------------------------------#
-# Write merged file separate .RData files
-save(hh_merge, file="destination.path/hh_merge.RData")
-save(per_merge, file="destination.path/per_merge.RData")
-rm()
+# write files
 
+if (destination.path != "not_save") {
+  if (format == "csv") {
+    save_path_personal <- paste(destination.path,"/per_merge.csv",sep="")
+    write.csv(per_merge, file=save_path_personal)  
+  }
+  
+  if (format == "RData") {
+    save_path_personal <- paste(destination.path,"/per_merge.RData",sep="")
+    save(per_merge, file=save_path_personal)
+  }
+  
+  if (format == "spss") {
+    library(foreign)
+    save_path_personal_datafile <- paste(destination.path,"/per_merge.txt",sep="")
+    save_path_personal_codefile <- paste(destination.path,"/per_merge.sps",sep="")
+    write.foreign(per_merge,  
+                  codefile=save_path_personal_codefile,
+                  datafile=save_path_personal_datafile, 
+                  package="SPSS") 
+  }
+  
+  if (format == "sas") {
+    library(foreign)
+    save_path_personal_datafile <- paste(destination.path,"/per_merge.txt",sep="")
+    save_path_personal_codefile <- paste(destination.path,"/per_merge.sas",sep="")
+    write.foreign(per_merge,  
+                  codefile=save_path_personal_codefile,
+                  datafile=save_path_personal_datafile, 
+                  package="SAS") 
+  }
+  
+  if (format == "stata") {
+    library(foreign)
+    save_path_personal_datafile <- paste(destination.path,"/per_merge.csv",sep="")
+    save_path_personal_codefile <- paste(destination.path,"/per_merge.do",sep="")
+    write.foreign(per_merge,  
+                  codefile=save_path_personal_codefile,
+                  datafile=save_path_personal_datafile, 
+                  package="Stata") 
+  }
 }
+
+
+rm(list=setdiff(ls(), "per_merge"))
+per_merge
+}
+
+
+
+#' Merge eu-silc household level longitudinal raw .csv datasets, household_register and household_data
+#'
+#' @param origin.path A string. Specify the path where to load the original csv files.
+#' @param destination.path A string. Specify the path where to save the merged file. value=/code{"not_save"} does not save the merged file
+#'
+#' @return data.frame
+#'
+#' @export
+#' @examples # merge_longitudinal(origin.path = "~/eusilc_raw", destination.path="~/eusilc_merged")
+#' @author Markus Kainu <markuskainu(at)gmail.com> 
+
+
+merge_longi_household <- function(origin.path,destination.path,format) {
+  
+  if(!exists("origin.path")) stop("origin.path not defined")
+  if(!exists("destination.path")) stop("destination.path not defined")
+  if(!exists("format")) stop("format not defined")
+  if(!(format %in% c("csv","RData","spss","sas","stata"))) stop("Wrong format. Use csv,RData,spss,sas,stata")
+  
+  # suppressPackageStartupMessages(library(data.table))
+  # suppressPackageStartupMessages(library(bit64))
+  
+  
+  # household register
+  path_household_register <- paste(origin.path,"/d_file.csv",sep="")
+  # hh_reg <- fread(path_household_register, header = T, sep = ',')
+  hh_reg <- read.csv(path_household_register, header = T, sep = ',')
+  hh_reg$HH_ID_Y <- factor(paste(hh_reg$DB010,hh_reg$DB020,hh_reg$DB030, sep="_"))
+  hh_reg$HH_ID <- factor(paste(hh_reg$DB020,hh_reg$DB030, sep="_"))
+  # personal data
+  path_household_data <- paste(origin.path,"/h_file.csv",sep="")
+  # hh_data <- fread(path_household_data, header = T, sep = ',')
+  hh_data <- read.csv(path_household_data, header = T, sep = ',')
+  hh_data$HH_ID_Y <- factor(paste(hh_data$HB010,hh_data$HB020,hh_data$HB030, sep="_"))
+  hh_data$HH_ID <- factor(paste(hh_data$HB020,hh_data$HB030, sep="_"))
+  
+    
+  #--------------------------------------------------------------------#
+  # merge data
+  hh_merge <- merge(hh_reg,hh_data,by="HH_ID_Y", all=TRUE)
+  #--------------------------------------------------------------------#
+  # write files
+  
+  if (destination.path != "not_save") {
+    if (format == "csv") {
+      save_path_household <- paste(destination.path,"/per_merge.csv",sep="")
+      write.csv(per_merge, file=save_path_household)  
+    }
+    
+    if (format == "RData") {
+      save_path_household <- paste(destination.path,"/per_merge.RData",sep="")
+      save(per_merge, file=save_path_household)
+    }
+    
+    if (format == "spss") {
+      library(foreign)
+      save_path_household_datafile <- paste(destination.path,"/per_merge.txt",sep="")
+      save_path_household_codefile <- paste(destination.path,"/per_merge.sps",sep="")
+      write.foreign(per_merge,  
+                    codefile=save_path_household_codefile,
+                    datafile=save_path_household_datafile, 
+                    package="SPSS") 
+    }
+    
+    if (format == "sas") {
+      library(foreign)
+      save_path_household_datafile <- paste(destination.path,"/per_merge.txt",sep="")
+      save_path_household_codefile <- paste(destination.path,"/per_merge.sas",sep="")
+      write.foreign(per_merge,  
+                    codefile=save_path_household_codefile,
+                    datafile=save_path_household_datafile, 
+                    package="SAS") 
+    }
+    
+    if (format == "stata") {
+      library(foreign)
+      save_path_household_datafile <- paste(destination.path,"/per_merge.csv",sep="")
+      save_path_household_codefile <- paste(destination.path,"/per_merge.do",sep="")
+      write.foreign(per_merge,  
+                    codefile=save_path_household_codefile,
+                    datafile=save_path_household_datafile, 
+                    package="Stata") 
+    }
+  }
+  
+  
+  rm(list=setdiff(ls(), "per_merge"))
+  per_merge
+}
+
